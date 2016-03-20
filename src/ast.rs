f02ff37e95a084ffd5e07dd8b0e7e66d686ba6e2
Martin Evans <martindevans@gmail.com>
2016-03-20 00:57:24 +0000

    renamed sql_syntax_tree.rs to ast.rs Moved tests into a separate test module and added a load more tests Begun work to implement column definitions (constraints are WIP)
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ src/ast.rs
@@ -0,0 +1,105 @@
+//Core components
+#[derive(PartialEq, Eq, Debug)]
+pub struct TableName<'a> {
+    pub name: &'a str,
+    pub schema_name: Option<&'a str>
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct ColumnConstraint<'a> {
+    pub name: Option<&'a str>,
+    pub constraint: ColumnConstraintType
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct PrimaryKeyConstraint {
+    pub ascending: Option<bool>,
+    pub conflict: ConflictClause,
+    pub auto_increment: Option<bool>
+}
+
+//https://www.sqlite.org/syntax/column-constraint.html
+#[derive(PartialEq, Eq, Debug)]
+pub enum ColumnConstraintType {
+    PrimaryKey(PrimaryKeyConstraint),
+    NotNull(ConflictClause),
+    Unique(ConflictClause),
+    //Check,
+    //Default,
+    Collate(CollateFunction),
+    //Foreign
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum ConflictClause {
+    Rollback,
+    Abort,
+    Fail,
+    Ignore,
+    Replace
+}
+
+//https://www.sqlite.org/datatype3.html#collation
+#[derive(PartialEq, Eq, Debug)]
+pub enum CollateFunction {
+    Binary,
+    NoCase,
+    RightTrim
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct ColumnDefinition<'a> {
+    pub name: &'a str,
+    pub type_def: Option<Type<'a>>,
+    pub constraints: Vec<ColumnConstraint<'a>>
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Type<'a> {
+    pub name: &'a str,
+    pub num1: Option<i32>,
+    pub num2: Option<i32>
+}
+
+//https://www.sqlite.org/syntax/sql-stmt.html
+#[derive(PartialEq, Eq, Debug)]
+pub enum Statement<'a> {
+    Plain(StatementBody<'a>),
+    Explain(StatementBody<'a>),
+    ExplainQueryPlan(StatementBody<'a>)
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum StatementBody<'a> {
+
+    //https://www.sqlite.org/syntax/alter-table-stmt.html
+    AlterTableRenameTo(TableName<'a>, TableName<'a>),
+    AlterTableAddColumn(TableName<'a>, ColumnDefinition<'a>),
+
+    /* Analyze(i32),
+    Attach(i32),
+    Begin(i32),
+    Commit(i32),
+    CreateIndex(i32),
+    CreateTable(i32),
+    CreateTrigger(i32),
+    CreateView(i32),
+    CreateVirtualTable(i32),
+    Delete(i32),
+    DeleteLimited(i32),
+    Detach(i32),
+    DropIndex(i32),
+    DropTable(i32),
+    DropTrigger(i32),
+    DropView(i32),
+    Insert(i32),
+    Pragma(i32),
+    Reindex(i32),
+    Release(i32),
+    Rollback(i32),
+    Savepoint(i32),
+    Select(i32),
+    Update(i32),
+    UpdateLimited(i32),
+    Vacuum(i32), */
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,212 @@
 pub mod sql_parser;
-pub mod sql_syntax_tree;
+pub mod ast;
 
 #[cfg(test)]
-fn test_it(sql: &'static str) {
-    match sql_parser::parse_Statement(sql) {
-        Ok(_) => {},
-        Err(err) => {
-            assert!(false, "{:#?}", err);
+mod tests {
+    use std::fmt::{ Debug };
+    use std::cmp::{ Eq };
+
+    use super::sql_parser as sql_parser;
+    use super::ast::{
+        Statement,
+        StatementBody,
+        TableName,
+        ColumnDefinition,
+        Type,
+        PrimaryKeyConstraint,
+        ColumnConstraintType,
+        ColumnConstraint,
+        ConflictClause
+    };
+
+    //Helpers
+    fn parse_to_ast<'a, P, R, E>(input: &'a str, parser: P, expected: R)
+        where P : Fn(&'a str) -> Result<R, E>,
+              E : Debug,
+              R : Debug, 'a,
+              R : Eq
+    {
+        match parser(input) {
+            Ok(r) => {
+                if r != expected {
+                    assert!(false, "Parsing \"{}\" got \n{:#?} expected \n{:#?}", input, r, expected)
+                }
+            }
+            Err(err) => assert!(false, "{:#?}", err)
         }
     }
-}
 
-#[test]
-fn it_works() {
+    fn parse_to_fail<'a, P, R, E>(input: &'a str, parser: P)
+        where P : Fn(&'a str) -> Result<R, E>,
+              E : Debug,
+              R : Debug, 'a,
+              R : Eq
+    {
+        match parser(input) {
+            Ok(r) => {
+                assert!(false, "Expected failure, succeeded with {:#?}", r)
+            }
+            Err(_) => { /* Expected failure :) */ }
+        }
+    }
 
-    test_it("ALTER TABLE schema.table RENAME TO foo");
-    test_it("ALTER TABLE table ADD COLUMN foo");
-    test_it("EXPLAIN ALTER TABLE table ADD COLUMN foo");
-    test_it("EXPLAIN QUERY PLAN ALTER TABLE table ADD COLUMN foo");
+    fn sql_statement_to_ast<'a>(sql: &'a str, expected: Statement<'a>) {
+        parse_to_ast::<'a>(sql, sql_parser::parse_Statement, expected);
+    }
 
+    #[test]
+    fn parse_table_name_no_schema() {
+        parse_to_ast("tableName", sql_parser::parse_TableName, TableName {
+            name: "tableName",
+            schema_name: None
+        });
+    }
+
+    #[test]
+    fn parse_table_name_with_invalid_name() {
+        parse_to_fail("#", sql_parser::parse_TableName);
+    }
+
+    #[test]
+    fn parse_table_name_with_schema() {
+        parse_to_ast("schema.tableName", sql_parser::parse_TableName, TableName {
+            name: "tableName",
+            schema_name: Some("schema")
+        });
+    }
+
+    #[test]
+    fn parse_table_name_with_invalid_schema() {
+        parse_to_fail("#.tableName", sql_parser::parse_TableName);
+    }
+
+    #[test]
+    fn parse_type_with_name() {
+        parse_to_ast("INT", sql_parser::parse_Type, Type {
+            name: "INT",
+            num1: None,
+            num2: None,
+        });
+    }
+
+    #[test]
+    fn parse_type_with_name_and_num() {
+        parse_to_ast("INT(1)", sql_parser::parse_Type, Type {
+            name: "INT",
+            num1: Some(1),
+            num2: None,
+        });
+    }
+
+    #[test]
+    fn parse_type_with_name_and_nums() {
+        parse_to_ast("INT(1,2)", sql_parser::parse_Type, Type {
+            name: "INT",
+            num1: Some(1),
+            num2: Some(2),
+        });
+    }
+
+    #[test]
+    fn alter_table_rename() {
+        sql_statement_to_ast("ALTER TABLE schema.table RENAME TO foo",
+            Statement::Plain(
+                StatementBody::AlterTableRenameTo(
+                    TableName {
+                        schema_name: Some("schema"),
+                        name: "table"
+                    },
+                    TableName {
+                        schema_name: None,
+                        name: "foo"
+                    },
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn alter_table_add_column() {
+        sql_statement_to_ast("ALTER TABLE table ADD COLUMN column",
+            Statement::Plain(
+                StatementBody::AlterTableAddColumn(
+                    TableName {
+                        schema_name: None,
+                        name: "table"
+                    },
+                    ColumnDefinition {
+                        name: "column",
+                        type_def: None,
+                        constraints: Vec::new()
+                    }
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn explain_alter_table_add_column() {
+        sql_statement_to_ast("EXPLAIN ALTER TABLE table ADD COLUMN column",
+            Statement::Explain(
+                StatementBody::AlterTableAddColumn(
+                    TableName {
+                        schema_name: None,
+                        name: "table"
+                    },
+                    ColumnDefinition {
+                        name: "column",
+                        type_def: None,
+                        constraints: Vec::new()
+                    }
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn parse_explain_query_plan_wraps_correct_ast() {
+        sql_statement_to_ast("EXPLAIN QUERY PLAN ALTER TABLE table ADD COLUMN column",
+            Statement::ExplainQueryPlan(
+                StatementBody::AlterTableAddColumn(
+                    TableName {
+                        schema_name: None,
+                        name: "table"
+                    },
+                    ColumnDefinition {
+                        name: "column",
+                        type_def: None,
+                        constraints: Vec::new()
+                    }
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn parse_alter_with_constraint() {
+        sql_statement_to_ast("EXPLAIN ALTER TABLE table ADD COLUMN column INT(1, 2) PRIMARY KEY ASC ON CONFLICT ABORT AUTOINCREMENT",
+            Statement::Explain(
+                StatementBody::AlterTableAddColumn(
+                    TableName {
+                        schema_name: None,
+                        name: "table"
+                    },
+                    ColumnDefinition {
+                        name: "column",
+                        type_def: None,
+                        constraints: vec![
+                            ColumnConstraint {
+                                name: None,
+                                constraint: ColumnConstraintType::PrimaryKey(PrimaryKeyConstraint {
+                                    ascending: true,
+                                    conflict: ConflictClause::Abort,
+                                    auto_increment: true
+                                })
+                            }
+                        ]
+                    }
+                )
+            )
+        );
+    }
 }
